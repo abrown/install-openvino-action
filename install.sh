49bd8f7db18fffee8d6c03449edc0abb9ca1db0d
Andrew Brown <andrew.brown@intel.com>
2022-05-10 14:58:09 -0700

    Set default installation to OpenVINO 2022.1.0 (#3)

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ set -e
 #  version=2021.4.752
 #  version_year=2021
 if [ "$#" -ne 1 ]; then
-    version="2021.4.752"
+    version="2022.1.0"
 else
     version="$1"
 fi
@@ -18,7 +18,7 @@ version_year="${version_parts[0]}"
 #  os_name=ubuntu
 #  os_version=20.04
 #  os_version_year=20
-eval $(source /etc/os-release; echo os_name="$ID"; echo os_version="$VERSION_ID";)
+eval $(source /etc/os-release; echo os_name="$ID"; echo os_version="$VERSION_ID"; echo os_version_codename="$VERSION_CODENAME")
 IFS='.' read -ra os_version_parts <<< "$os_version"
 os_version_year="${os_version_parts[0]}"
 
@@ -27,14 +27,14 @@ os_version_year="${os_version_parts[0]}"
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Retrieve the OpenVINO checksum.
-curl -sSL https://apt.repos.intel.com/openvino/$version_year/GPG-PUB-KEY-INTEL-OPENVINO-$version_year > $scriptdir/GPG-PUB-KEY-INTEL-OPENVINO-$version_year
-echo "5f5cff8a2d26ba7de91942bd0540fa4d $scriptdir/GPG-PUB-KEY-INTEL-OPENVINO-$version_year" > $scriptdir/CHECKSUM
+curl -sSL https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB > $scriptdir/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
+echo "818253460e4e4a045cc92ddff13fbc94 $scriptdir/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB" > $scriptdir/CHECKSUM
 md5sum --check $scriptdir/CHECKSUM
 
 # Add the OpenVINO repository (DEB-specific for now).
-sudo apt-key add $scriptdir/GPG-PUB-KEY-INTEL-OPENVINO-$version_year
-echo "deb https://apt.repos.intel.com/openvino/$version_year all main" | sudo tee /etc/apt/sources.list.d/intel-openvino-$version_year.list
+sudo apt-key add $scriptdir/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
+echo "deb https://apt.repos.intel.com/openvino/$version_year $os_version_codename main" | sudo tee /etc/apt/sources.list.d/intel-openvino-$version_year.list
 sudo apt update
 
 # Install the OpenVINO package.
-sudo apt install -y intel-openvino-runtime-$os_name$os_version_year-$version
+sudo apt install -y openvino-$version
