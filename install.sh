6dd72e35e689f163a31a295f63ba60b76684c1a7
Brian Jones <brian.j.jones@intel.com>
2022-10-07 14:29:05 -0700

    Saving openvino folder to /opt/intel where its expected

--- install.sh
+++ install.sh
@@ -5,4 +5,7 @@ set -e
 FILENAME=l_openvino_toolkit_ubuntu20_2022.2.0.7713.af16ea1d79a_x86_64
 wget -q --no-check-certificate https://storage.openvinotoolkit.org/repositories/openvino/packages/2022.2/linux/${FILENAME}.tgz
 tar -zxf ${FILENAME}.tgz
-source ${FILENAME}/setupvars.sh
+sudo mkdir -p /opt/intel
+sudo mv ${FILENAME} /opt/intel/openvino_2022
+sudo ln -s /opt/intel/openvino_2022/ /opt/intel/openvino
+source /opt/intel/openvino/setupvars.sh
