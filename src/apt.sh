8dee88639c343e6586de251c029f4a3dcd9d7cd2
Andrew Brown <andrew.brown@intel.com>
2023-10-02 16:35:25 -0700

    Update APT checksum

--- src/apt.sh
+++ src/apt.sh
@@ -17,7 +17,7 @@ action_dir="$(dirname $( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 &
 
 # Retrieve the OpenVINO signing key.
 curl -sSL https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB > $action_dir/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
-echo "818253460e4e4a045cc92ddff13fbc94 $action_dir/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB" > $action_dir/CHECKSUM
+echo "3e1b48a4dbbbaea2b0f442b9ca15821e $action_dir/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB" > $action_dir/CHECKSUM
 md5sum --check $action_dir/CHECKSUM
 sudo apt-key add $action_dir/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
 
