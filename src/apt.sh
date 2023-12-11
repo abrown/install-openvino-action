bd99ee30b352aab0fded105a1d0c36d0df6dd5ff
Andrew Brown <andrew.brown@intel.com>
2023-12-11 13:53:30 -0800

    Use latest version with APT (#25)
    According to the OpenVINO [docs], we should now be able to install OpenVINO with the `apt.sh` script. To do so, we need to use the OS release name (e.g., `ubuntu22`) instead of codename (e.g., `jammy`) when we create the `deb` file for APT. Also, we need to install slightly different prerequisites when it comes to TBB: Ubuntu 20.04 installations require `libtbb2` but all later versions, like Ubuntu 22.04 need `libtbb12`. We can verify this in the downloaded `install_dependencies/install_openvino_dependencies.sh` script.
    
    [docs]: https://docs.openvino.ai/2023.2/openvino_docs_install_guides_installing_openvino_apt.html#installing-openvino-runtime

--- src/apt.sh
+++ src/apt.sh
@@ -5,11 +5,9 @@ set -e
 # Install OpenVINO using APT packages. This script expects to be passed certain variables from
 # `main.js` (it is easier to calculate them there), but for local use these could be set manually.
 
-# TODO: the latest release (v2023.0.0) is not yet available as an APT package; update this when it
-# is.
-version=${version:-2022.3.0}
-version_year=${version_year:-2022}
-os_codename=${os_codename:-focal}
+version=${version:-2023.2.0}
+version_year=${version_year:-2023}
+os=${os:-ubuntu22}
 
 # Determine the directory of this script. E.g.:
 #  action_dir=/some/directory
@@ -21,13 +19,7 @@ echo "3e1b48a4dbbbaea2b0f442b9ca15821e $action_dir/GPG-PUB-KEY-INTEL-SW-PRODUCTS
 md5sum --check $action_dir/CHECKSUM
 sudo apt-key add $action_dir/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
 
-# Warn about the OS codename.
-if [ "$os_codename" == "jammy" ]; then
-    echo "ERROR: OpenVINO has released no 'jammy' (Ubuntu 22.04) packages; use the 'focal' (Ubuntu 20.04) packages instead"
-    exit 1
-fi
-
 # Add the OpenVINO repository and install the OpenVINO DEB package.
-echo "deb https://apt.repos.intel.com/openvino/$version_year $os_codename main" | sudo tee /etc/apt/sources.list.d/intel-openvino-$version_year.list
+echo "deb https://apt.repos.intel.com/openvino/$version_year $os main" | sudo tee /etc/apt/sources.list.d/intel-openvino-$version_year.list
 sudo apt update
 sudo apt install -y openvino-$version
