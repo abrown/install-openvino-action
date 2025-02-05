7cdd25b5cdff9b992b2eb3b283b447c2f779f095
Andrew Brown <andrew.brown@intel.com>
2025-02-04 16:37:57 -0800

    Update default version to 2024.6.0

--- src/apt.sh
+++ src/apt.sh
@@ -5,7 +5,7 @@ set -e
 # Install OpenVINO using APT packages. This script expects to be passed certain variables from
 # `main.js` (it is easier to calculate them there), but for local use these could be set manually.
 
-version=${version:-2024.4.0}
+version=${version:-2024.6.0}
 version_year=${version_year:-2024}
 release=${release:-ubuntu22}
 
